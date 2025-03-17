2ca61e0382b38a68e80a4b02ae65459bca466174
Siddharth-magesh <siddharthmagesh007@gmail.com>
2025-03-17 12:02:39 +0530

    added css integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  integrations: [tailwind()],
   site: 'https://Siddharth-magesh.github.io',
   base: '/Ossarth-Website/',
   output: 'static'
